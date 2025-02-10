04b8c19c9b5e3560ad4f7f2e09d41bd722035f5c
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-02-10 11:55:32 +0900

    test: 6장

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
-fun main() {
-    println("Hello, World!")
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun add(arr: ArrayList<Int>): ArrayList<Int> {
+    arr.add(3)
+    return arr
 }
+
+fun main() {
+    var arr: ArrayList<Int> = arrayListOf(1, 2)
+    println(add(arr))
+    println(arr);
+}
+
