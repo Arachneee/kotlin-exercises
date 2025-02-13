a9565cb60ddcc2f843472caaefce996da5f0c366
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-02-14 01:41:21 +0900

    test: 10, 11장

--- src/main/kotlin/essentials/classes/Data.kt
+++ src/main/kotlin/essentials/classes/Data.kt
@@ -1,5 +1,18 @@
 package essentials.classes.data
 
+data class Person(val name: String, val age: Int)
+
 fun main() {
     // Write your code here
+    val john = Person("John", 30)
+    println(john)
+    val jane = john.copy(name = "Jane")
+    println(jane)
+    val jane2 = Person("Jane", 30)
+    println(jane.equals(jane2))
+    println(john.hashCode())
+    println(jane.hashCode())
+    println(jane2.hashCode())
+    val (name, age) = jane
+    println("$name, $age")
 }
